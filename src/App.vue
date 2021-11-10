4c4c118f148f77f75f7ca5a152dff2630b1d7f50
edo-2313 <32812884+edo2313@users.noreply.github.com>
2021-11-10 15:11:54 +0100

    Added logo

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,11 @@
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="./assets/logo.png"
           transition="scale-transition"
           width="40"
         />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-toolbar-title>VoiceMote</v-toolbar-title>
       </div>
 
       <v-spacer></v-spacer>
